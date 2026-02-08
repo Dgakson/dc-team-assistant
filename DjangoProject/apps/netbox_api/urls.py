31ace4ba4e917a9dd43afea49b309da540b0aa15
Dgakson <www.jenya96@gmail.com>
2026-02-08 21:06:57 +0300

    update urls

--- DjangoProject/apps/netbox_api/urls.py
+++ DjangoProject/apps/netbox_api/urls.py
@@ -11,13 +11,13 @@ from apps.netbox_api.views.assets import (
 
 urlpatterns = [
     ### ASSETS GET
-    path('assets/', AssetsListView.as_view(), name='assets_list'),
-    path('assets/<int:asset_id>/', AssetDetailView.as_view(), name='assets_detail'),
+    path('assets_list/', AssetsListView.as_view(), name='assets_list'),
+    path('asset/<int:asset_id>/', AssetDetailView.as_view(), name='asset_detail'),
     path('asset_types/', AssetsTypeListView.as_view(), name='asset_types_list'),
     ### ASSETS POST
-    path('assets/create/', AssetsCreateView.as_view(), name='assets_create'),
-    path('assets/repair/', AssetsRepairView.as_view(), name='assets_repair'),
-    path('assets/modernization/', AssetsModernizationView.as_view(), name='assets_modernization'),
+    path('create/', AssetsCreateView.as_view(), name='assets_create'),
+    path('repair/', AssetsRepairView.as_view(), name='assets_repair'),
+    path('modernization/', AssetsModernizationView.as_view(), name='assets_modernization'),
 
     path('site_location/', SitesLocationListView.as_view(), name='site_location_list'),
 ]
--- DjangoProject/config/urls.py
+++ DjangoProject/config/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.netbox_api.urls')),
+    path('inventory/', include('apps.netbox_api.urls')),
     path('auth/', include('apps.users.urls')),
 ]
